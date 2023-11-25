79f3db9919459c631b75ea7262866a9952bbc511
wejdanA <lol.lol.1417@hotmail.com>
2023-11-25 18:23:24 +0300

    clean the code

--- src/helper/sendEmail.ts
+++ src/helper/sendEmail.ts
@@ -17,21 +17,16 @@ type EmailDataType = {
 }
 export const sendEmail = async (emailData: EmailDataType) => {
   try {
-    console.log('user', dev.app.smtpUserName)
-    console.log('password', dev.app.smtpPassword)
     const mailOptions = {
       from: dev.app.smtpUserName,
       to: emailData.email,
       subject: emailData.subject,
       html: emailData.html,
     }
-    console.log('user from  mailoptoion', mailOptions.from)
-    console.log('to from  mailoptoion', mailOptions.to)
 
     const info = await transporter.sendMail(mailOptions)
     console.log('message was sent', info.response)
   } catch (error) {
-    console.log('error with email', error)
     throw error
   }
 }
