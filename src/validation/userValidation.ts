f04ea6d1a7548f8e1e7636d998352907c37556dc
wejdanA <lol.lol.1417@hotmail.com>
2023-12-03 09:05:44 +0300

    enhance validation

--- src/validation/userValidation.ts
+++ src/validation/userValidation.ts
@@ -33,6 +33,43 @@ export const userRegistrationValidation = [
     .withMessage('password must be at least 5 characters'),
 ]
 
+export const userUpdatenValidation = [
+  check('firstName')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('First name must not be empty')
+    .isLength({ min: 3, max: 20 })
+    .withMessage('First name must be at least 3 charachters')
+    .isLength({ max: 20 })
+    .withMessage('First name must be less than 20 characters'),
+  check('lastName')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Last name must not be empty')
+    .isLength({ min: 3, max: 20 })
+    .withMessage('Last name must be at least 3 charachters')
+    .isLength({ min: 3, max: 20 })
+    .withMessage('Last name must be at least 3 characters')
+    .isLength({ max: 20 })
+    .withMessage('Last name must be less than 20 characters'),
+  check('email')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Email must not be empty')
+    .isEmail()
+    .withMessage('Email is not a valid'),
+  check('password')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Password must not be empty')
+    .isLength({ min: 5 })
+    .withMessage('password must be at least 5 characters'),
+]
+
 export const userLoginValidation = [
   check('email')
     .trim()
