725110ed39e112e3297814bf34784310675cce8f
Maram Alfaraj <105658778+MaramLA@users.noreply.github.com>
2023-12-03 07:12:05 +0300

    Update ordersRouter.ts

--- src/routers/ordersRouter.ts
+++ src/routers/ordersRouter.ts
@@ -6,14 +6,18 @@ import { isAdmin, isLoggedIn, isNotAdmin } from '../middlewares/authentication'
 const router = Router()
 
 //GET --> get all user orders
-router.get('/:userId([0-9a-fA-F]{24})', isLoggedIn, controller.getOrdersForUser)
+router.get('/', isLoggedIn, isNotAdmin, controller.getOrdersForUser)
+
 //GET --> get all orders for an admin
 router.get('/all-orders', isLoggedIn, isAdmin, controller.getOrdersForAdmin)
+
 //GET --> create an order
 router.post('/process-payment', isLoggedIn, isNotAdmin, controller.handleProcessPayment)
+
 //DELETE --> delete a single order by ID
-router.delete('/:id', isLoggedIn, isAdmin, controller.deleteOrder)
+router.delete('/:id([0-9a-fA-F]{24})', isLoggedIn, isAdmin, controller.deleteOrder)
+
 //PUT --> update a single order by ID
-router.put('/:id', isLoggedIn, isAdmin, controller.updateOrder)
+router.put('/:id([0-9a-fA-F]{24})', isLoggedIn, isAdmin, controller.updateOrder)
 
 export default router
